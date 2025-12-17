d38aabf139ca353d955dd4f220c897ed43383fcd
V1n007 <amroshadeed1@gmail.com>
2025-12-17 21:40:24 +0000

    Initial commit - source code only

---
+++ android/app/src/main/java/com/shaw/wukongbosstracker/MainActivity.java
@@ -0,0 +1,5 @@
+package com.shaw.wukongbosstracker;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ preload.js
@@ -0,0 +1,2 @@
+// This file can be used to expose Node.js APIs to your renderer process
+// in a secure way. For this simple checklist, it can remain empty.
